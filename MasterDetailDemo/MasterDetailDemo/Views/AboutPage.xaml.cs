7d0d5e4c094e76c1b43f89f6f183934241ac848e
jorgecensi <jorgecensi@gmail.com>
2017-03-28 21:24:51 -0300

    commit inicial

---
+++ MasterDetailDemo/MasterDetailDemo/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace MasterDetailDemo.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
